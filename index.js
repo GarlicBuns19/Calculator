f569bdba6f86bb5f7be590cced4cb9f56ca2be43
GarlicBuns19 <planterx26@gmail.com>
2022-06-08 14:06:53 +0200

    help

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ function buttonDisplay(num){
 function buttonClear(){
     document.getElementById('output').innerHTML = '';
 }
+
+    let firstOperand = buttonDisplay(a,);
+
+    let operator = document.querySelector('#multiply','#backwards','#minus','#add');
+
+    let secondOperand = buttonDisplay('',b);
+
+    switch(operator) {
+        case '+':
+            total = eval(fstOp.concat(ops,secOp));
+    }
+
+    document.getElementById('#equal')
